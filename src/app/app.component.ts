9dc9b01e45b7f65298160b447ead9fb8e2f3a2cc
Kuba Buda <j.buda@onix.pl>
2018-05-17 13:13:19 +0200

    toggle matSidenav viewchild by service in the same component works on FF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarService } from './services/sidebar.service';
 import { Title } from '@angular/platform-browser';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,22 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent implements OnInit {
   isDarkTheme = false;
   title = 'ONIX Web client';
+  @ViewChild('sidenav') sidenav: MatSidenav
 
   constructor(private router: Router,
+    private sidebar: SidebarService,
     private titleService: Title) {}
 
   ngOnInit() {
     this.router.navigateByUrl('/barcode')
     this.titleService.setTitle( this.title );
+    this.sidebar.toggleMenu.subscribe(() => {
+      this.sidenav.toggle()
+    });
+  }
+
+  toggleSidenav() {
+    this.sidebar.toggle();
   }
 
 }
