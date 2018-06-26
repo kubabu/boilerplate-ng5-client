84c7670cf36ba99f28d3196d6468160df81fc7a7
Kuba Buda <j.buda@onix.pl>
2018-06-26 15:17:22 +0200

    snackbar message on HTTP error

--- src/app/services/interceptors/auth-interceptor.service.ts
+++ src/app/services/interceptors/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
 import 'rxjs/add/operator/do';
 import { Observable } from 'rxjs/Observable';
 
@@ -13,6 +14,7 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(
         private authStore: AuthenticationStoreService,
         private logoutService: LogoutService,
+        public snackBar: MatSnackBar,
     ) { }
 
     intercept(req: HttpRequest<any>,
@@ -31,9 +33,12 @@ export class AuthInterceptor implements HttpInterceptor {
             if (err instanceof HttpErrorResponse) {
                 if (err.status === 0) {
                     // API is unavailable
+                    this.snackBar.open('Serwer niedostępny', 'OK');
+                    // this.logoutService.logout();
                 } else if (err.status === 401) {
-                    // unauthorized. JWT expired or other problem. User needs to relogin
+                    // unauthorized. JWT expired without handlinf or other problem. User needs to relogin
                     console.log('Unauthorized. JWT expired or other problem. User needs to relogin');
+                    this.snackBar.open('Uwierzytelnianie nie działa', 'OK');
                     this.logoutService.logout();
                 }
                 // do error handling here
